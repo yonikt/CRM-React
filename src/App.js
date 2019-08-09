297dee52f33a547750f8818b6f2fbd0e7194d0cd
yoni kiat <yoni24ster@gmail.com>
2019-08-09 16:56:22 +0300

    fg

--- src/App.js
+++ src/App.js
@@ -7,14 +7,25 @@ import Analytics from './components/Analytics';
 const axios = require('axios')
 
 
-
-
 class App extends Component {
 
-  constructor() {
-    super()
-    this.state = { users: [] }
+  constructor(props) {
+    super(props)
+    this.state = { users: [], rowsToDisplay: 17 }
+    this.showMore = this.showMore.bind(this)
+    this.showLess = this.showLess.bind(this)
   }
+
+  showMore() {
+    this.setState({ rowsToDisplay: this.state.rowsToDisplay + 17 })
+  }
+
+  showLess() {
+    if (this.state.rowsToDisplay > 1) {
+      this.setState({ rowsToDisplay: this.state.rowsToDisplay - 17 })
+    }
+  }
+
 
   async getUsers() {
     return axios.get("http://localhost:9988/data")
@@ -26,24 +37,25 @@ class App extends Component {
   }
 
 
-
   render() {
     return (
       <Router>
-        <div className="App">
 
-          <ul>
-            <li><Link className="active" to="/clients">CLIENTS</Link></li>
-            <li><Link to="/actions">ACTIONS</Link></li>
-            <li> <Link to="/analytics">ANALYTICS</Link></li>
-          </ul>
+        <ul>
+          <li><Link className="active" to="/clients">CLIENTS</Link></li>
+          <li><Link to="/actions">ACTIONS</Link></li>
+          <li> <Link to="/analytics">ANALYTICS</Link></li>
+        </ul>
 
-          <Route exact path="/" render={() => <Redirect to="/clients" />} />
-          <Route exact path="/clients" render={() => <Clients data={this.state.users} />} />
-          <Route exact path="/actions" render={() => <Actions users={this.state.users} />} />
-          <Route exact path="/analytics" render={() => <Analytics users={this.state.users} />} />
+        <Route exact path="/" render={() => <Redirect to="/clients" />} />
+        <Route exact path="/clients" render={() => <Clients data={this.state.users.slice(0, this.state.rowsToDisplay)} />} />
+        <Route exact path="/actions" render={() => <Actions users={this.state.users} />} />
+        <Route exact path="/analytics" render={() => <Analytics users={this.state.users} />} />
 
-        </div>
+        {this.state.users.length > 0 ? <span> <br></br> <button id="next" onClick={this.showLess}>«</button> </span> : null}
+        {this.state.rowsToDisplay > 0 && this.state.users.length > 0 ? <span id="counting"> {this.state.rowsToDisplay - 16} - {this.state.rowsToDisplay} </span> : null}
+        {this.state.users.length > 0 ? <button id="previous" onClick={this.showMore}>»</button> : null}
+
       </Router>
     )
   }
