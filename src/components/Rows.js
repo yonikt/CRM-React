fc30b008377cca284fa309148009e51aeea38d40
yoni kiat <yoni24ster@gmail.com>
2019-08-09 13:07:06 +0300

    DF

--- src/components/Rows.js
+++ src/components/Rows.js
@@ -19,15 +19,18 @@ class Rows extends Component {
     <table class="fl-table" onClick={this.togglePopup.bind(this)}>
         <tbody>
         <tr>
-            <td>{i.name.split(" ")[0]}</td>
+            <td>{i.name.split(" ")[0]}</td> 
             <td>{i.name.split(" ")[1]}</td>
             <td>{i.country}</td>
             <td>{i.firstContact.slice(3, 10)}</td>
             <td>{i.emailType === null ? "." : i.emailType}</td>
             <td>{i.sold === "true" ? <img src="https://png.pngtree.com/svg/20161202/da573c8d9d.svg" width="10" alt="not found"></img> : "-"}</td>
             <td>{i.owner}</td>
+            
         </tr>
+        
         </tbody>
+       
     </table>
                 {this.state.showPopup ? <Popup text={i} closePopup={this.togglePopup.bind(this)} /> : null}
             </div>
